2acb0b7ee18878c43003397b39b7e0f5ae1a4e28
vahid mohamadi <31723327+vaeid@users.noreply.github.com>
2024-05-22 13:08:17 +0330

    complete css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+export default function App() {
+  return (
+    <div className='tab'>
+      <div className='tab__header'>
+        <button>Item 1</button>
+        <button>Item 2</button>
+        <button>Item 3</button>
+      </div>
+      <div className='tab__content'></div>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
-function App() {
- return <h1>hiiiiiiiiiiiiiiiii</h1>
-}
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
